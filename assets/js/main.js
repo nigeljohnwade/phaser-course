30863dd709da2387f1ae43b4f396daee43d892ca
nigeljohnwade <nigeljohnwade@gmail.com>
2020-07-16 19:15:05 +0100

    Physics

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,18 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene:{
+    scene: {
         preload: preload,
         create: create,
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: {
+                y: 5,
+            },
+        },
     },
 };
 
@@ -18,17 +27,19 @@ function preload() {
 
 function create() {
     // button with origin set to top left of image
-    var button = this.add.image(100,100,'button1');
+    var button = this.add.image(100, 100, 'button1');
     button.setOrigin(0, 0);
     // sprite with origin set to center which would be default
     // sprite has an animation component attached
-    var sprite = this.add.sprite(300,200,'button1');
-    sprite.setOrigin(0.5,0.5);
+    var sprite = this.add.sprite(300, 200, 'button1');
+    sprite.setOrigin(0.5, 0.5);
     // loads frames from the spritesheet as images by specifying frame number
     // these will not have an animation component attached
-    var chest = this.add.image(100,400,'items');
-    var chest = this.add.image(200,400,'items', 1);
-    var chest = this.add.image(300,400,'items', 2);
-    var chest = this.add.image(400,400,'items', 3);
-    var chest = this.add.image(500,400,'items', 4);
+    var chest = this.add.image(100, 400, 'items');
+    var chest = this.add.image(200, 400, 'items', 1);
+    var chest = this.add.image(300, 400, 'items', 2);
+    var chest = this.add.image(400, 400, 'items', 3);
+    var chest = this.add.image(500, 400, 'items', 4);
+
+    this.physics.add.image(500, 100, 'button1');
 }
