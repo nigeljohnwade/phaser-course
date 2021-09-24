e4afda1ea34f2b5e96734c906222b3b519288f8c
nigeljohnwade <nigeljohnwade@gmail.com>
2021-09-24 21:35:34 +0100

    Title scene

--- assets/js/Scenes/BootScene.js
+++ assets/js/Scenes/BootScene.js
@@ -5,6 +5,7 @@ class BootScene extends Phaser.Scene{
 
     preload() {
         this.load.image('button1', 'assets/images/ui/blue_button01.png');
+        this.load.image('button2', 'assets/images/ui/blue_button02.png');
         // loads an image and specifies each sprites frame dimensions
         this.load.spritesheet('items', 'assets/images/items.png', {frameWidth: 32, frameHeight: 32});
         this.load.spritesheet('characters', 'assets/images/characters.png', {frameWidth: 32, frameHeight: 32});
@@ -12,6 +13,6 @@ class BootScene extends Phaser.Scene{
     }
 
     create(){
-        this.scene.start('Game');
+        this.scene.start('Title');
     }
 }
--- assets/js/Scenes/TitleScene.js
+++ assets/js/Scenes/TitleScene.js
@@ -7,6 +7,21 @@ class TitleScene extends Phaser.Scene{
 
     }
     create() {
-        this.button = this.add.image(100, 100, 'button1');
+        this.titleText = this.add.text(this.scale.width / 2, this.scale.height / 2, 'Zenva MMORPG', {fontSize: '64px', fill: '#fff'});
+        this.titleText.setOrigin(0.5)
+        this.button = this.add.image(this.scale.width / 2, this.scale.height* 0.65, 'button1');
+        this.button.setInteractive();
+        this.button.on('pointerdown', (e) =>{
+            console.log(e, 'pointerdown');
+            this.scene.start('Game');
+        });
+        this.button.on('pointerover', (e) =>{
+            console.log(e, 'pointerover');
+            this.button.setTexture('button2');
+        });
+        this.button.on('pointerout', (e) =>{
+            console.log(e, 'pointerout');
+            this.button.setTexture('button1');
+        });
     }
 }
